6a5209add20c2d3ec32b22add76812b86c5f43cf
John B. Roberts <johnbroberts@gmail.com>
2025-06-15 15:40:03 -0400

    npm setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
     },
   },
   server: {
-    port: 5173,
+    port: 3000,
     open: true,
   },
   build: {
